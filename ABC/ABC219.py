9009e839180bbcba3fcfc84363c5e342ea3251ed
kazuhiro <hatano.kazuhiro817@gmail.com>
2022-09-12 22:07:36 +0900

    add ABC219:C

--- ABC/ABC219.py
+++ ABC/ABC219.py
@@ -46,8 +46,7 @@ def B():
 
 
 """
-例題はOKなのに通らない
-ソートが想定（辞書順）と違う？
+考え方はあってたが、変換表を間違えていた
 """
 def C_WA():
     x = input() 
@@ -80,12 +79,58 @@ def C_WA():
         print(word)
 
 
-x = input() 
-n = int(input())
-s = [input() for _ in range(n)]
+def C_ans():
+    x = input() 
+    n = int(input())
+    s = [input() for _ in range(n)]
 
+    #変換表の作成
+    import string
+    d = {}
+    for i in range(26):
+        nxt = chr(i + ord('a'))
+        d[x[i]] = nxt
+
+    trans = []
+    for i in range(n):
+        t = ""
+        for c in s[i]:
+            t += d[c]
+        trans.append([t, s[i]])
+
+    trans = sorted(trans)
+    for i in range(n):
+        print(trans[i][1])
 
 
 
 # def D():
+def C():
+    x = input() 
+    n = int(input())
+    s = [input() for _ in range(n)]
 
+    #変換表の作成
+    import string
+    d = {}
+    d_rev = {}
+    a = string.ascii_lowercase
+    for i in range(len(x)):
+        d[x[i]] = a[i]#####
+        d_rev[a[i]] = x[i]#####
+
+    #変換
+    s_trans = []
+    for i in range(n):
+        word = ""
+        for c in s[i]:
+            word += d[c]
+        s_trans.append(word)
+
+    #逆変換
+    s_trans = sorted(s_trans)
+    for i in range(n):
+        word = ""
+        for c in s_trans[i]:
+            word += d_rev[c]
+        print(word)
