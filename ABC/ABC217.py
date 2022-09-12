014fcb9f3194c938febab13dc8661ab04d6131f6
kazuhiro <hatano.kazuhiro817@gmail.com>
2022-09-12 09:48:26 +0900

    add ABC217, ABC218

---
+++ ABC/ABC217.py
@@ -0,0 +1,44 @@
+'''
+# 文字列を受け取る場合
+s = input() 
+
+# 1つの整数を入力
+n = int(input())
+
+# 複数の整数を入力
+a, b = map(int, input().split())
+
+# listで入力を受け取り
+a = list(map(int, input().split()))
+
+# Yes/Noテンプレ
+ans = True
+ans = False
+print("Yes" if ans else "No")
+'''
+
+def A():
+    s,t = input().split()
+    x = sorted([s, t])
+
+    if s==x[0]:
+        ans = True
+    else:
+        ans = False
+    print("Yes" if ans else "No")
+
+
+def B():
+    s = []
+    for _ in range(3):
+        s.append(input())
+
+    x = ["ABC", "ARC", "AGC", "AHC"]
+    diff = set(s) ^ set(x)
+    print(list(diff)[0])
+
+# def C():
+
+
+# def D():
+
---
+++ ABC/ABC218.py
@@ -0,0 +1,42 @@
+'''
+# 文字列を受け取る場合
+s = input() 
+
+# 1つの整数を入力
+n = int(input())
+
+# 複数の整数を入力
+a, b = map(int, input().split())
+
+# listで入力を受け取り
+a = list(map(int, input().split()))
+
+# Yes/Noテンプレ
+ans = True
+print("Yes" if ans else "No")
+'''
+
+def A():
+    n = int(input())
+    s = input()
+
+    if s[n-1]=="o":
+        ans = True
+    else:
+        ans = False
+    print("Yes" if ans else "No")
+
+
+def B():
+    p = list(map(int, input().split()))
+    s = ""
+    for i in range(len(p)):
+        s += chr(p[i]-1+97)
+    print(s)
+
+
+# def C():
+
+
+# def D():
+
