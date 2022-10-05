6e387a3be9f86f9d3c312b39e93254b8d70273bc
kazuhiro <hatano.kazuhiro817@gmail.com>
2022-10-05 23:10:17 +0900

    fix ABC270

--- ABC/ABC270.py
+++ ABC/ABC270.py
@@ -62,55 +62,78 @@ def B():
 深さ優先探索（DFS）
 DFSは木構造と相性が良い、らしい
 """
-# def C():
-n, x, y = map(int, input().split())
+def C_TLE():
+    #再帰回数の上限を変更
+    import sys
+    sys.setrecursionlimit(1000000)
 
-to = [[] for i in range(n+1)]
-for i in range(n-1):
-    a, b = map(int, input().split())
-    to[a].append(b)
-    to[b].append(a)
+    n, x, y = map(int, input().split())
+    x -= 1
+    y -= 1
+
+    g = [[] for _ in range(n)]
+    for i in range(n-1):
+        u, v = map(int, input().split())
+        u -= 1
+        v -= 1
+        g[u].append(v)
+        g[v].append(u)
+
+    """
+    v: 現在のノード
+    p: 親ノード (1つ前のノード)
+    """
+    def dfs(v, p):
+        if v==x:
+            ans.append(v)
+            return True
+        for u in g[v]:
+            if u==p:
+                continue
+            if dfs(u, v):
+                ans.append(v)
+                return True
+        return False
+
+    ans = []
+    dfs(y, -1)
+    print(" ".join([str(a+1) for a in ans]))
 
 
+"""
+貪欲法では解けないようになっている問題らしい
+"""
+def D_WA():
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    a = sorted(a)
 
+    takahashi = 0
+    aoki = 0
 
-# dfs(y)
-
-print()
-    
-
-# def D():
-# n, k = map(int, input().split())
-# a = list(map(int, input().split()))
-# a = sorted(a)
-
-# takahashi = 0
-# aoki = 0
-
-# from bisect import bisect_left
-# i = 0
-# while n>=a[0]:
-#     less_n = bisect_left(a, n)
-#     if less_n<k and a[less_n]==n:
-#         less_n = less_n
-#     else:
-#         less_n -= 1
-    
-#     if i%2==0:
-#         if less_n>0:
-#             takahashi += a[less_n]
-#             n -= a[less_n]
-#         else:
-#             takahashi += a[0]
-#             n -= a[0]
-#     else:
-#         if less_n>0:
-#             aoki += a[less_n]
-#             n -= a[less_n]
-#         else:
-#             aoki += a[0]
-#             n -= a[0]
-#     i += 1
-
-# print(takahashi)
+    from bisect import bisect_left
+    i = 0
+    while n>=a[0]:
+        less_n = bisect_left(a, n)
+        if less_n<k and a[less_n]==n:
+            less_n = less_n
+        else:
+            less_n -= 1
+        
+        if i%2==0:
+            if less_n>0:
+                takahashi += a[less_n]
+                n -= a[less_n]
+            else:
+                takahashi += a[0]
+                n -= a[0]
+        else:
+            if less_n>0:
+                aoki += a[less_n]
+                n -= a[less_n]
+            else:
+                aoki += a[0]
+                n -= a[0]
+        i += 1
+    print(takahashi)
 
