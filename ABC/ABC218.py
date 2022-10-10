b04c8d4aa472675bc0ec3055d2fd2c30b7a67bfa
kazuhiro <hatano.kazuhiro817@gmail.com>
2022-10-10 22:44:41 +0900

    add ABC218 C

--- ABC/ABC218.py
+++ ABC/ABC218.py
@@ -35,7 +35,71 @@ def B():
     print(s)
 
 
-# def C():
+"""
+https://qiita.com/u2dayo/items/35df8435c7e79fd07eeb#c%E5%95%8F%E9%A1%8Cshapes
+これで緑ってまじですか
+"""
+def C_ans():
+    def count_sharp(s):
+        ret = 0
+        for s_row in s:
+            ret += s_row.count("#")
+        return ret
+
+    def find_first_sharp(s):
+        n = len(s[0])
+        for row in range(n):
+            for col in range(n):
+                if s[row][col]=="#":
+                    return row, col
+
+    def is_same(s, t, dr, dc):
+        n = len(s[0])
+        for row in range(n):
+            for col in range(n):
+                row_s = row + dr
+                col_s = col + dc
+                if 0 <= row_s < n and 0 <= col_s < n:
+                    if s[row_s][col_s] != t[row][col]:
+                        return False
+                else:
+                    if t[row][col]=="#":
+                        #グリッドから # がはみ出ているため
+                        return False
+        return True
+
+    def rot(s):
+        return list(zip(*s[::-1]))
+
+    def judge(s, t):
+        # #の数が同じか
+        if count_sharp(s) != count_sharp(t):
+            return False
+        #回転用のfor
+        for _ in range(4):
+            #sの1番左上の#の位置
+            sr, sc = find_first_sharp(s)
+            #tの1番左上の#の位置
+            tr, tc = find_first_sharp(t)
+            #平行移動する量を計算
+            dr = sr - tr
+            dc = sc - tc
+            
+            if is_same(s, t, dr, dc):
+                return True
+            #tを90度回転 
+            t = rot(t)
+        return False
+
+    n = int(input())
+    s = []
+    for i in range(n):
+        s.append(list(input()))
+    t = []
+    for i in range(n):
+        t.append(list(input()))
+
+    print("Yes" if judge(s, t) else "No")
 
 
 
