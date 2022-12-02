8a9253d83a7a6016e4dbd8312a56824bca054c64
kazuhiro <hatano.kazuhiro817@gmail.com>
2022-12-02 21:29:37 +0900

    fix section_2

--- kyopro-tessoku/section_2.py
+++ kyopro-tessoku/section_2.py
@@ -19,6 +19,9 @@ print("Yes" if ans else "No")
 ans = " ".join([str(x) for x in a])
 '''
 
+"""
+累積和
+"""
 def A06():
     from itertools import accumulate
     n, q = map(int, input().split())
@@ -53,3 +56,174 @@ def B06():
             print("draw")
 
 
+"""
+累積和 & 差分を管理
+"""
+def A07():
+    from itertools import accumulate
+    d = int(input())
+    n = int(input())
+
+    #前日比を記録
+    a = [0]*(d+2)#0日目と(d+1)日目を追加
+    for i in range(n):
+        l, r = map(int, input().split())
+        a[l] += 1
+        a[r+1] -= 1
+
+    #前日比の累積和でその日の参加人数が出る
+    s = list(accumulate(a))
+    for i in range(d):
+        print(s[i+1])
+
+
+def B07():
+    from itertools import accumulate
+    t = int(input())
+    n = int(input())
+
+    a = [0]*(t+1)
+    for i in range(n):
+        l, r = map(int, input().split())
+        a[l] += 1
+        a[r] -= 1
+
+    s = list(accumulate(a))
+    for i in range(t):
+        print(s[i])
+
+"""
+2次元の累積和
+"""
+def A08():
+    h, w = map(int, input().split())
+    #0行目と0列目に0パディングすると後で簡単になる
+    pad = 1
+    max_h = h+pad
+    max_w = w+pad
+    
+    x = [[0]*(max_w) for i in range(max_h)]
+    for i in range(h):
+        row = list(map(int, input().split()))
+        for j in range(w):
+            x[i+1][j+1] = row[j]
+
+    #2次元の累積和
+    z = [[0]*(max_w) for i in range(max_h)]
+
+    #横方向の累積和
+    for i in range(1, h+1):
+        for j in range(1, w+1):
+            z[i][j] += z[i][j-1] + x[i][j]
+
+    #縦方向の累積和
+    for j in range(1, w+1):
+        for i in range(1, h+1):
+            z[i][j] += z[i-1][j]
+
+    q = int(input())
+    for k in range(q):
+        a, b, c, d = map(int, input().split())
+        ans = z[c][d] + z[a-1][b-1] - z[c][b-1] - z[a-1][d] 
+        print(ans)
+
+
+def B08():
+    n = int(input())
+    max_x = 1509
+    max_y = 1509
+
+    a = [[0]*(max_x) for i in range(max_y)]
+    for i in range(n):
+        x, y = map(int, input().split())
+        a[y][x] += 1
+
+    #2次元の累積和
+    z = [[0]*(max_x) for i in range(max_y)]
+
+    #横方向の累積和
+    for i in range(1, max_y):
+        for j in range(1, max_x):
+            z[i][j] += z[i][j-1] + a[i][j]
+
+    #縦方向の累積和
+    for j in range(1, max_x):
+        for i in range(1, max_y):
+            z[i][j] += z[i-1][j]
+
+    q = int(input())
+    for k in range(q):
+        a, b, c, d = map(int, input().split())
+        ans = z[d][c] + z[b-1][a-1] - z[d][a-1] - z[b-1][c] 
+        print(ans)
+
+
+"""
+2次元の累積和 & 差分を管理
+"""
+def A09():
+    h, w, n = map(int, input().split())
+    max_h = h+2
+    max_w = w+2
+    x = [[0]*(max_w) for i in range(max_h)]
+
+    for i in range(n):
+        a, b, c, d = map(int, input().split())
+        x[a][b] += 1
+        x[a][d+1] -= 1
+        x[c+1][b] -= 1
+        x[c+1][d+1] += 1
+
+    #2次元の累積和
+    z = [[0]*(max_w) for i in range(max_h)]
+
+    #横方向の累積和
+    for i in range(1, h+1):
+        for j in range(1, w+1):
+            z[i][j] += z[i][j-1] + x[i][j]
+
+    #縦方向の累積和
+    for j in range(1, w+1):
+        for i in range(1, h+1):
+            z[i][j] += z[i-1][j]
+
+    for i in range(1, h+1):
+        print(" ".join([str(v) for v in z[i][1:-1]]))
+
+
+def B09():
+    n = int(input())
+    max_x = 1509
+    max_y = 1509
+    p = [[0]*(max_x) for i in range(max_y)]
+
+    #差分を記録
+    for i in range(n):
+        a, b, c, d = map(int, input().split())
+        a += 1
+        b += 1
+        c += 1
+        d += 1
+        p[a][b] += 1
+        p[c][d] += 1
+        p[a][d] -= 1
+        p[c][b] -= 1
+
+    #2次元の累積和
+    z = [[0]*(max_x) for i in range(max_y)]
+
+    #横方向
+    for i in range(1, max_y):
+        for j in range(1, max_x):
+            z[i][j] = z[i][j-1] + p[i][j]
+    #縦方向
+    for j in range(1, max_x):
+        for i in range(1, max_y):
+            z[i][j] += z[i-1][j]
+
+    ans = 0
+    for i in range(1, max_y):
+        for j in range(1, max_x):
+            if z[i][j] > 0:
+                ans += 1
+    print(ans)
