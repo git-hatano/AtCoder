2108cf47f9fc5fda8fb3cb4f49a11e81b6d699b6
kazuhiro <hatano.kazuhiro817@gmail.com>
2023-01-08 21:22:42 +0900

    add ABC284

--- ABC/ABC276.py
+++ ABC/ABC276.py
@@ -71,4 +71,126 @@ def C():
     print(ans)
 
 
-# def D():
+"""
+AC:18, WA:23
+sampleは通る
+"""
+def D_WA():
+    import sys
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+
+    #全部同じなら何もしない
+    same = True
+    for i in range(n-1):
+        if a[i]!=a[i+1]:
+            same = False
+    if same:
+        print(0)
+        sys.exit()
+
+    #2でも3でも割れない数字が入っていたら無理
+    for i in range(n):
+        if a[i]!=1 and a[i]%2!=0 and a[i]%3!=0:
+            print(-1)
+            sys.exit()
+
+    #その時の最小値に合わせることができるか見ていく? #math.log2(10**9) <= 30
+    mi = a[0]
+    while mi>=1:
+        cnt = 0
+        for i in range(1, n):
+            if a[i]//mi == a[i]/mi:
+                d = a[i]//mi
+                if d%3==0:
+                    cnt += d//3
+                    d %= 3
+                if d%2==0:
+                    cnt += d//2
+                    d %= 2
+                if d!=0:
+                    cnt = 0
+                    break
+            else:
+                cnt = 0
+                break
+        if cnt>0:
+            break
+        
+        if mi==1:
+            break
+        elif a[0]%3==0:
+            mi //= 3
+        elif a[0]%2==0:
+            mi //= 2
+        else:
+            break
+        
+    if cnt==0:
+        print(-1)
+    else:
+        print(cnt)
+
+
+def D_ans():
+    import math
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    #最大公約数
+    g = 0
+    for i in range(n):
+        g = math.gcd(g, a[i])
+
+    ans = 0
+    for i in range(n):
+        # なんで最大公約数で割っていい?
+        # 最大公約数に揃えると考えて、最大公約数になるための操作回数を求めている
+        a[i] //= g 
+        while a[i]%2==0:#2で割り続ける
+            a[i] //= 2
+            ans += 1
+        while a[i]%3==0:#3で割り続ける
+            a[i] //= 3
+            ans += 1
+        if a[i]!=1:#2,3で割り続けて1じゃなかったら無理
+            ans = -1
+            break
+    print(ans)
+
+
+def D_ans2():
+    import sys
+    n = int(input())
+    a = list(map(int, input().split()))
+    cnt2 = []
+    cnt3 = []
+    #2,3で割り続けてそもそも同じになるか確認
+    for i in range(n):
+        cnt = 0
+        while a[i]%2==0:#2で割り続ける
+            a[i] //= 2
+            cnt += 1
+        cnt2.append(cnt)
+        cnt = 0
+        while a[i]%3==0:#3で割り続ける
+            a[i] //= 3
+            cnt += 1
+        cnt3.append(cnt)
+
+    #2,3で割り続けた結果、同じにならないなら無理
+    for i in range(n-1):
+        if a[i]!=a[i+1]:
+            print(-1)
+            sys.exit()
+    ans = 0
+    #/2の操作回数
+    mi = min(cnt2)
+    for i in range(n):
+        ans += cnt2[i]-mi
+    #/3の操作回数
+    mi = min(cnt3)
+    for i in range(n):
+        ans += cnt3[i]-mi
+    print(ans)
---
+++ ABC/ABC284.py
@@ -0,0 +1,95 @@
+'''
+# 文字列を受け取る場合
+s = input() 
+
+# 1つの整数を入力
+n = int(input())
+
+# 複数の整数を入力
+a, b = map(int, input().split())
+
+# listで入力を受け取り
+a = list(map(int, input().split()))
+
+# Yes/Noテンプレ
+ans = True #ans = False
+print("Yes" if ans else "No")
+
+# リストの中身を文字列に
+ans = " ".join([str(x) for x in a])
+'''
+
+def A():
+    n = int(input())
+    s = [input() for _ in range(n)]
+    s.reverse()
+    for i in range(n):
+        print(s[i])
+
+
+def B():
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        ans = 0
+        for j in range(n):
+            if a[j]%2==1:
+                ans += 1
+        print(ans)
+
+
+class UnionFind_simple:
+    def __init__(self, n) -> None:
+        self.par = [-1]*n #最初は親がない
+        self.siz = [1]*n #最初はグループの頂点数が1
+    
+    #頂点xの親を返す関数
+    def root(self, x):
+        while True:
+            if self.par[x]==-1: #1個先(親)がなければここが親
+                break
+            x = self.par[x] #1個先(親)に進む
+        return x
+    
+    #要素uと要素vを統合する関数
+    def unite(self, u, v):
+        root_u = self.root(u)
+        root_v = self.root(v)
+        if root_u==root_v: #uとvが同じグループなら何もしない
+            return
+        if self.siz[root_u] < self.siz[root_v]:
+            self.par[root_u] = root_v
+            self.siz[root_v] += self.siz[root_u]
+        else:
+            self.par[root_v] = root_u
+            self.siz[root_u] += self.siz[root_v]
+    
+    #uとvが同じグループかを返す関数
+    def same(self, u, v):
+        return self.root(u)==self.root(v)
+
+def C():
+    n, m = map(int, input().split())
+    uf = UnionFind_simple(n)
+
+    for i in range(m):
+        u, v = map(int, input().split())
+        u -= 1
+        v -= 1
+        uf.unite(u, v)
+
+    ans = 0
+    for i in range(n):
+        if uf.par[i]==-1:
+            ans += 1
+    print(ans)
+
+
+# def D():
+t = int(input())
+for i in range(t):
+    n = int(input())
+
+
+
