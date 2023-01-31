eaec8a40469e3795f35cbe95c38cc1d85d61c3ac
kazuhiro <hatano.kazuhiro817@gmail.com>
2023-01-31 22:31:54 +0900

    fix ABC215

--- ABC/ABC215.py
+++ ABC/ABC215.py
@@ -58,5 +58,70 @@ def C():
     x = sorted(list(set(x)))
     print(x[k-1])
 
-# def D():
 
+"""
+2重ループでTLE. 答えは合ってそう
+"""
+def D_TLE():
+    import math
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    ans = []
+    for k in range(1, m+1):
+        ok = True
+        for i in range(n):
+            if math.gcd(a[i], k)!=1:
+                ok = False
+                break
+        if ok:
+            ans.append(k)
+    l = len(ans)
+    ans.sort()
+    print(l)
+    for i in range(l):
+        print(ans[i])
+
+
+"""
+kになれないものを消していく
+エラトステネスの篩の要領
+"""
+def factorization(n):
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append([i, cnt])
+    if temp!=1:
+        arr.append([temp, 1])
+    if arr==[]:
+        arr.append([n, 1])
+    return arr
+
+def D_ans():
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    #kの候補
+    size = max(max(a), m)
+    b = [True]*(size+1)
+
+    #素因数分解してkになれないものを削除
+    for i in range(n):
+        facts = factorization(a[i]) #素因数分解
+        for v in facts:
+            if v[0]!=1 and b[v[0]]==True: #一度計算したものは再計算しないようにしないとTLE
+                for j in range(v[0], size+1, v[0]): #素数の倍数も消す
+                    b[j] = False
+
+    #b[i]=Trueになっているものが答え
+    ans = []
+    for i in range(1, m+1):
+        if b[i]:
+            ans.append(i)
+    print(len(ans))
+    for k in ans:
+        print(k)
