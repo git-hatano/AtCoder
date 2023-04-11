18a4607e93d32d6d9b8b0f330e9c43b3929e32c0
kazuhiro <hatano.kazuhiro817@gmail.com>
2023-04-12 08:04:21 +0900

    fix ABC204

--- ABC/ABC204.py
+++ ABC/ABC204.py
@@ -79,5 +79,39 @@ def C():
     print(ans)
 
 
-# def D():
+"""
+いくつかの値を選んで作ることができるか？』は、部分和問題という有名な問題で、
+動的計画法
+
+dp[i][j]
+i個目まで決めて、sum(a)をjとできるか
+O(n*t)
+"""
+def D_ans():
+    n = int(input())
+    t = list(map(int, input().split()))
+    s = sum(t)
+
+    dp = [[False]*(s+1) for i in range(n+1)]
+    dp[0][0] = True
+
+    for i in range(n):
+        for j in range(s+1):
+            """
+            dp[i + 1][j]がTrueになるのは、
+            dp[i][j] が True（T[i]を使わない場合）
+            dp[i][j - t] が True（T[i]を使う場合）
+            のどちらかです
+            """
+            if dp[i][j]:
+                dp[i+1][j] = True
+            if j-t[i]>=0 and dp[i][j-t[i]]:
+                dp[i+1][j] = True
+
+    ans = 10**10
+    for i in range(s+1):
+        if dp[n][i]:
+            score = max(i, s-i) # 片方をiにすると、もう片方はs-i
+            ans = min(ans, score)
+    print(ans)
 
