bca77a7b27b9a0ca8a2bec878e7e65e8bff48243
kazuhiro <hatano.kazuhiro817@gmail.com>
2023-05-06 23:25:09 +0900

    fix ABC266

--- ABC/ABC266.py
+++ ABC/ABC266.py
@@ -72,16 +72,36 @@ def C():
     print("Yes" if ans else "No")
 
 
-# def D():
-n = int(input())
+"""
+多分DP、選ぶ選ばないの場合分けをしていく感じ？
+Ans: DPだった
 
-pos = 0
-point = 0
-for i in range(n):
-    t, x, a = map(int, input().split())
-    #移動できる
-    if abs(t-pos)>x:
-        pos = x
-        point += a
-    
-print()
+DP[x][t]
+高橋君が時刻tに座標xにいるときの、それまでに捕まえた大きさの合計の最大値
+"""
+def D():
+    m = 10**5 +5
+    n = int(input())
+    X = [0]*m
+    A = [0]*m
+    for i in range(n):
+        t, x, a = map(int, input().split())
+        X[t] = x
+        A[t] = a
+
+    INF = 10**18
+    dp = [[-INF]*5 for i in range(m)]
+    dp[0][0] = 0
+    for i in range(m-1):
+        ni = i+1
+        for j in range(5):
+            for nj in range(j-1, j+2):
+                if nj<0 or 5<=nj:
+                    continue
+                dp[ni][nj] = max(dp[ni][nj], dp[i][j])
+        dp[ni][X[ni]] += A[ni]
+
+    ans = -INF
+    for j in range(5):
+        ans = max(ans, dp[m-1][j])
+    print(ans)
